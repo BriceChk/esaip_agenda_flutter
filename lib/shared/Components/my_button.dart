c23fb5e30401b2baa5200d8c783d0bcde7170fe4
Madere0 <59331413+Madere0@users.noreply.github.com>
2021-04-10 17:13:44 +0200

    Component button

---
+++ lib/shared/Components/my_button.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final String text;
+  final Color colorBorder;
+  final Color colorButton;
+  final Color colorText;
+  final Color colorOverlay;
+  final double borderWidth;
+  final GestureTapCallback onPressed;
+  final Color colorShadow;
+
+  MyButton({required this.text, required this.colorBorder, required this.colorButton, required this.colorText,
+    required this.colorOverlay, required this.onPressed, this.borderWidth = 2, required this.colorShadow});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      //TODO : ajouter un parametre pour faire des shadows couleur
+      style: ButtonStyle(
+        elevation: MaterialStateProperty.all<double>(0),
+        overlayColor: MaterialStateProperty.all<Color>(colorOverlay),
+        backgroundColor: MaterialStateProperty.all<Color>(colorButton),
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(50),
+            side: BorderSide(
+              color: colorBorder,
+              width: borderWidth,
+            ),
+          ),
+        ),
+      ),
+      onPressed: onPressed,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 60, vertical: 15),
+        child: Text(
+          text,
+          style: TextStyle(
+            fontSize: 20,
+            color: colorText,
+            letterSpacing: 0.25,
+          ),
+        ),
+      ),
+    );
+  }
+}

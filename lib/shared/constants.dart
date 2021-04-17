a61723e2ef81cfb81e28eabab4af835fc2c3deb9
Madere0 <59331413+Madere0@users.noreply.github.com>
2021-04-17 18:10:34 +0200

    change constants color

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -8,9 +8,9 @@ const COLOR_PURPLE_CARD = Color(0xffD4C1EC);
 const COLOR_RED_CARD_BUTTON = Color(0xffFF898F);
 const COLOR_RED_TEXT = Color(0xffA95156);
 const COLOR_RED_CARD = Color(0xffFFACB0);
-const COLOR_GREEN_CARD_BUTTON = Color(0xff2DA34B);
-const COLOR_GREEN_TEXT = Color(0xffD6F6DD);
-const COLOR_GREEN_CARD = Color(0xffB1EDC0);
+const COLOR_GREEN_CARD_BUTTON = Color(0xffB1EDC0);
+const COLOR_GREEN_TEXT = Color(0xff2DA34B);
+const COLOR_GREEN_CARD = Color(0xffD6F6DD);
 const COLOR_GREY = Color(0xff4E4E4E);
 const COLOR_WHITE_GREY = Color(0xffF4F4F4);
 

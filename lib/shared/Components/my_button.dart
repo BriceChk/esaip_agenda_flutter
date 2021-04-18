b56b07b0ce454052d8c7215c1aafc7b79fc0c71c
Madere0 <59331413+Madere0@users.noreply.github.com>
2021-04-18 16:47:15 +0200

    Change the button components

--- lib/shared/Components/my_button.dart
+++ lib/shared/Components/my_button.dart
@@ -17,32 +17,37 @@ class MyButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      //TODO : ajouter un parametre pour faire des shadows couleur
-      style: ButtonStyle(
-        elevation: MaterialStateProperty.all<double>(0),
-        overlayColor: MaterialStateProperty.all<Color>(colorOverlay),
-        backgroundColor: MaterialStateProperty.all<Color>(colorButton),
-        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-          RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(50),
-            side: BorderSide(
-              color: colorBorder,
-              width: borderWidth,
+    return Container(
+      width: MediaQuery.of(context).size.width - 60,
+      child: ElevatedButton(
+        //TODO : ajouter un parametre pour faire des shadows couleur
+        style: ButtonStyle(
+          elevation: MaterialStateProperty.all<double>(0),
+          overlayColor: MaterialStateProperty.all<Color>(colorOverlay),
+          backgroundColor: MaterialStateProperty.all<Color>(colorButton),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(50),
+              side: BorderSide(
+                color: colorBorder,
+                width: borderWidth,
+              ),
             ),
           ),
         ),
-      ),
-      onPressed: onPressed,
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 60, vertical: 15),
-        child: Text(
-          text,
-          style: TextStyle(
-            fontSize: 20,
-            fontFamily: FONT_NUNITO,
-            color: colorText,
-            letterSpacing: 0.25,
+        onPressed: onPressed,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+          child: FittedBox(
+            child: Text(
+              text,
+              style: TextStyle(
+                fontSize: 20,
+                fontFamily: FONT_NUNITO,
+                color: colorText,
+                letterSpacing: 0.25,
+              ),
+            ),
           ),
         ),
       ),
